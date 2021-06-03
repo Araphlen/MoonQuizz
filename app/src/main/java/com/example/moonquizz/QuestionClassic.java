f18284a899f15e62e5deb5bddccff1a60f7c807c
Amraphel <Amraphel@netcourrier.com>
2021-06-03 15:30:47 +0200

    test

---
+++ app/src/main/java/com/example/moonquizz/QuestionClassic.java
@@ -0,0 +1,14 @@
+package com.example.moonquizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionClassic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_classic);
+    }
+}
